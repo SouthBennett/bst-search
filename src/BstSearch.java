e7b2b2324eb1246bbbbcb53ba807666872e05e2e
SouthBennett <146032836+SouthBennett@users.noreply.github.com>
2026-03-01 12:29:06 -0800

    add sample tree in test.java and first attempt at solving the method. unsuccessful

--- src/BstSearch.java
+++ src/BstSearch.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BstSearch {
     /**
      * Returns whether a binary search tree contains a given value.
@@ -17,6 +20,35 @@ public class BstSearch {
      * @throws NullPointerException if target is null
      */
     public static <T extends Comparable<T>> boolean contains(BinaryTreeNode<T> root, T target) {
+        if (root == null) return false;
+
+        if (target == null) throw new NullPointerException();
+
+        Queue<BinaryTreeNode<T>> queue = new LinkedList<>();
+
+        BinaryTreeNode<T> current = root;
+
+        queue.add(current);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                current = queue.poll();
+
+                if (current.data == target) {
+                    return true;
+                }
+
+                if (current.left != null) {
+                    queue.add(current.left);
+                }
+
+                if (current.right != null) {
+                    queue.add(current.right);
+                }
+            } 
+        }
         return false;
     }
 }
--- src/BstSearchTest.java
+++ src/BstSearchTest.java
@@ -2,5 +2,28 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class BstSearchTest {
+  private BinaryTreeNode<Integer> binaryTree() {
+    BinaryTreeNode<Integer> root = 
+      new BinaryTreeNode<Integer>(10,
+        new BinaryTreeNode<>(5,
+          new BinaryTreeNode<>(3),
+          new BinaryTreeNode<>(7)
+        ),
+        new BinaryTreeNode<>(15,
+          new BinaryTreeNode<>(12),
+          new BinaryTreeNode<>(20)
+          )
+        );
+    return root;    
+  }
 
+  @Test
+  void testValuesExists() {
+    BinaryTreeNode<Integer> root = binaryTree();
+
+    assertTrue(BstSearch.contains(root, 8));
+    assertTrue(BstSearch.contains(root, 3));
+    assertTrue(BstSearch.contains(root, 20));
+
+  }
 }
